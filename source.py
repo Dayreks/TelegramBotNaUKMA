d554927722f98fb440929d521d580e27f8f7a437
mdmasya <52499153+mdmasya@users.noreply.github.com>
2021-03-23 18:34:37 +0200

    another try to change api token and run on my branch

--- source.py
+++ source.py
@@ -3,15 +3,11 @@ import json
 source = json.load(open("source/source.json", encoding='utf-8'))
 btn_json = source["btn"]
 msg_json = source["msg"]
-<<<<<<< HEAD
-# API_TOKEN = '1647854818:AAHkxDcGaklU_DtBbkKRcwV-QGyuUtGVgnI'
-API_TOKEN = '1672955809:AAHbwvoMU9mSGdNJDCPtK63M86qDxoXtmfI' #(Max)
-#API_TOKEN = '1698570040:AAGvmUeVtpTKw18dn-VlTUAXDT04vWp-uhI'
-=======
 
 coef = json.load(open("source/coef.json", encoding='utf-8'))
 
-API_TOKEN = '1647854818:AAHkxDcGaklU_DtBbkKRcwV-QGyuUtGVgnI'
+API_TOKEN = '1672955809:AAHbwvoMU9mSGdNJDCPtK63M86qDxoXtmfI' #(Max)
+# API_TOKEN = '1647854818:AAHkxDcGaklU_DtBbkKRcwV-QGyuUtGVgnI'
 # API_TOKEN = '1698570040:AAGvmUeVtpTKw18dn-VlTUAXDT04vWp-uhI'
 
 
@@ -25,4 +21,3 @@ class UserState(Enum):
     NULL_STATE ="NULL_STATE"
     EXAM_PTS_ENTRY_STATE = "EXAM_PTS_ENTRY_STATE"
     
->>>>>>> master
