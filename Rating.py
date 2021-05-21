7092ef2504a890b491955ad36164ca9824408bce
Bohdan Arkhypchuk <arkhypchuk.b@gmail.com>
2021-05-21 11:49:35 +0300

    Final fixes in texts and some buttons

--- Rating.py
+++ Rating.py
@@ -27,7 +27,7 @@ def calculate_final_rate(faculty, speciality, rate1, rate2, rate3, rate4):
         rate1 = float(rate1)
         rate2 = float(rate2)
         rate3 = float(rate3)
-        rate4 = float(rate4)
+        rate4 = float(rate4)*200/12
     except:
         return "Неправильні дані"
     rates = [rate1, rate2, rate3, rate4]
